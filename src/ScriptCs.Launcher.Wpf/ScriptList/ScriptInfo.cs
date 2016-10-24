8bd9aa626efd34d76424b0c3047e9dfbf29bc9c4
Amadeusz Wieczorek <git@amadeusw.com>
2016-10-24 11:57:57 -0700

    adds viewmodel for script list

---
+++ src/ScriptCs.Launcher.Wpf/ScriptList/ScriptInfo.cs
@@ -0,0 +1,19 @@
+﻿using ScriptCs.Launcher.Wpf.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptCs.Launcher.Wpf.ScriptList
+{
+    class ScriptInfo : ViewModelBase
+    {
+        public string Path { get; set; }
+        public string Host { get; set; }
+        public string Arguments { get; set; }
+        public bool LogOutput { get; set; }
+        public string Icon { get; set; }
+        public string Shortcut { get; set; }
+    }
+}
